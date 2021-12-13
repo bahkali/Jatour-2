9075167c63a175fece6a705ef32767ab1a98a279
kaly <kalimamadou@gmail.com>
2021-12-13 03:02:00 -0800

    Jatour built the Trip API Controller and build couple request

---
+++ Controllers/TripsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Persistence.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JaTour.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public TripsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Trip>>> GetTrips()
+        {
+           return await _context.Trips.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Trip>> GetTrip(Guid id) {
+            return await _context.Trips.FindAsync(id);
+        }
+    }
+}
